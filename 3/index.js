2b8044755b1facc813d56ab58f010530983fcd35
Yuka <cas_siopeia@live.com>
2019-04-23 21:34:28 +0300

    commit

--- 3/index.js
+++ 3/index.js
@@ -1,9 +1,85 @@
 $(function () {
+    class Ball {
+        top = 0;
+        left = 0;
+        directionX = 'RIGTH';
+        directionY = 'DOWN';
+        canvas$;
+        ball$;
+        constructor(canvas, ball) {
+            this.canvas$ = canvas;
+            this.ball$ = ball;
+        }
 
-    var indent = 30;
-    $('button').on('click', function () {
-        $('.box').css('left', indent + 'px');
-        indent += 30;
+        run() {
+            var positionLeft = this.ball$.position().left;
+            var positionTop = this.ball$.position().top;
+            var borderWidth = this.canvas$.width() - this.ball$.width();
+            var borderHeight = this.canvas$.height() - this.ball$.height();
+
+            if (positionLeft == borderWidth) {
+                this.directionX = 'LEFT';
+            } else if (positionLeft == 0) {
+                this.directionX = 'RIGTH';
+            }
+
+            if (positionTop == borderHeight) {
+                this.directionY = 'UP';
+            } else if (positionTop == 0) {
+                this.directionY = 'DOWN';
+            }
+
+            if (this.directionX == 'RIGTH') {
+                $('.box').css('left', this.left++ + 'px');
+            } else if (this.directionX == 'LEFT') {
+                $('.box').css('left', this.left-- + 'px');
+            }
+
+
+            if (this.directionY == 'DOWN') {
+                $('.box').css('top', this.top++ + 'px');
+            } else if (this.directionY == 'UP') {
+                $('.box').css('top', this.top-- + 'px');
+            }
+        }
+
+    }
+    var ballArray = [];
+
+    // var canvasWidth = $('.canvas').width();
+    // var boxWidth = $('.box').width();
+    // var canvasHeight = $('.canvas').height();
+    // var boxHeight = $('.box').height();
+
+    // var indentW = 0;
+    // var indentY = 0;
+    // var directionX = 'RIGTH';
+    // var directionY = 'DOWN';
+    var intervalId;
+
+
+    $('.run').click(function () {
+        if (!intervalId) {
+            intervalId = setInterval(function () {
+                ballArray.forEach(function (ball) {
+                    ball.run();
+                })
+            }, 4);
+        }
     })
 
-})
+    $('.stop').click(function () {
+        clearInterval(intervalId);
+        intervalId = null;
+    })
+
+    $('.ball').click(function () {
+        var canvas$ = $('.canvas');
+        var ball$ = $('<div class="box red">1</div>');
+        var ball = new Ball(canvas$, ball$);
+        ballArray.push(ball);
+        canvas$.append(ball$);
+    })
+});
+
+
